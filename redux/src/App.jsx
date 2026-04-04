6bf2159de962d132e4eea15159c195ada58976f7
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-04 23:24:26 +0530

    implementing features

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './redux/features/couterSlice'
 
 const App = () => {
 
   const dispatch = useDispatch() // dispatch action , work from button click
-  const count = useSelector() // ui shpowing , changes to show after button click 
+
+  // const count = useSelector() // ui shpowing , changes to show after button click 
+
+  const count = useSelector((state)=>state.counter.value) // couter is name on store , it shows initial values
 
   return (
     <div>
-      <h1>0</h1>
+      {/* <h1>0</h1> */}
+      <h1>{count}</h1>
 
       <button onClick={()=>{
-
+        dispatch(increment()) // call the function to use
       }}>Increment</button>
+
       <button onClick={()=>{
-        
+      dispatch(decrement())        
       }}>drcrement</button>
     </div>
   )
