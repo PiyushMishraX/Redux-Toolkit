8b40d8a2126e1fda2f48971a6d68125d24abf666
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-04 23:36:55 +0530

    input field and argumenets passign to reducers

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { decrement, increment, incrementByAmount } from './redux/features/couterSlice'
 
@@ -10,6 +10,9 @@ const App = () => {
 
   const count = useSelector((state)=>state.counter.value) // couter is name on store , it shows initial values
 
+  // teo way binding the input field
+  const [num, setNum] = useState(5)
+
   return (
     <div>
       {/* <h1>0</h1> */}
@@ -17,15 +20,32 @@ const App = () => {
 
       <button onClick={()=>{
         dispatch(increment()) // call the function to use
-      }}>Increment</button>
+      }}>
+        Increment
+        </button>
 
       <button onClick={()=>{
       dispatch(decrement())        
-      }}>drcrement</button>
+      }}>
+        drcrement
+        </button>
 
+      <input type="number"  
+      value={num}
+      onChange={(e)=>{
+        // console.log(e.target);
+        // console.log(e.target.value);
+        setNum(e.target.value)
+        
+      }}/>
       <button onClick={()=>{
-      dispatch(incrementByAmount(5))        
-      }}>Increase by 5</button>
+      // dispatch(incrementByAmount(5))        
+      // dispatch(incrementByAmount(num))   
+      // type cast so value willbe in form of number not string      
+      dispatch(incrementByAmount(Number(num)))        
+      }}>
+        Increase by Amount
+        </button>
     </div>
   )
 }
