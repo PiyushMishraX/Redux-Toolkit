356881f6ef35ca0685357433909f8841e8f45ee3
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-05 22:39:55 +0530

    api key importing method

--- project/src/api/mediaApi.js
+++ project/src/api/mediaApi.js
@@ -1,1 +1,7 @@
 // code from where we will call api and fetch api
+
+import axios from 'axios'
+
+// api key import method
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
+const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
