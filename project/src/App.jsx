0c8723cea68a7d7c149dd061ad5ab18f525501b2
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-05 23:07:51 +0530

    parasms

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,10 +1,23 @@
+import { fetchPhotos } from "./api/mediaApi"
 
 
 const App = () => {
+
+  function getPhotos(){
+    fetchPhotos()
+  }
   return (
     <div className="h-screen w-ful text-white  bg-gray-950">
       {/* <h1 className='text-6xl bg-red-600'>I am Piyush</h1> */}
       
+      {/* <button onClick={getPhotos}>Get photos</button> */}
+      {/* <button onClick={fetchPhotos}>Get photos</button>  */}
+      {/* to see fo to inspect -> network -> photos */}
+
+
+      <button onClick={()=>{
+        fetchPhotos('cat')
+      }}>Get photos</button> 
     </div>
   )
 }
--- project/src/api/mediaApi.js
+++ project/src/api/mediaApi.js
@@ -5,3 +5,17 @@ import axios from 'axios'
 // api key import method
 const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
+
+export function fetchPhotos(query,page =1 , per_page=20) {
+    // const res = axios.get('https://api.unsplash.com/search/photos') // to get photos
+    // <https://api.unsplash.com/search/photos?page=1&query=office>; rel="first", <https://api.unsplash.com/search/photos?page=1&query=office>; rel="prev", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="last", <https://api.unsplash.com/search/photos?page=3&query=office>; rel="next" // we use shorter method intead of hard code writing with {} // shorter and structured method
+
+
+    const res = axios.get('https://api.unsplash.com/search/photos',{
+        // pass query in params 
+        params:{query,page,per_page}, // passing query in params
+        headers:{}
+    }) // to get photos
+    console.log(res);
+    
+}
