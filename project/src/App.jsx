42c44f52df974be213190084fcf252a5b834055d
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-05 15:53:06 +0530

    project

---
+++ project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-6xl bg-red-600'>I am Piyush</h1>
+      
+    </div>
+  )
+}
+
+export default App
