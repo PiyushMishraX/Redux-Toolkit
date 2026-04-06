b6917e30e2040d35934ef3c5dbdd2007d5b99bef
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-06 23:24:56 +0530

    query search

--- project/src/components/ResultGrid.jsx
+++ project/src/components/ResultGrid.jsx
@@ -1,15 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPhotos, fetchVideos, fetchGIF } from "../api/mediaApi"; 
 // one level up from current --> ..
-import {setQuery, setLoading, setError , setResults } from '..redux/features/searchSlice'
+import {setQuery, setLoading, setError , setResults } from '../redux/features/searchSlice'
+import { useEffect } from "react";
 
 
 const ResultGrid = () => {
 
+    const {query, activeTab, results, loading, error} = useSelector((store)=>store.search)
+
+    const getData = async ()=>{
+        if(activeTab== 'photos'){
+            const data= await fetchPhotos(query)
+
+            console.log(data); // select photos then input query search then get data
+             
+        }
+    }
 
   return (
     <div>
-      
+      <button onClick={getData}>Get Data</button>
     </div>
   )
 }
