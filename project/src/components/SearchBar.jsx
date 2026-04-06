246eebb98e11e00e9b6778ee25368000a3a869f4
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-06 21:24:21 +0530

    search bar

--- project/src/components/SearchBar.jsx
+++ project/src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div>
+      <form >
+        <input type="text" placeholder='Search anything..' />
+        <button>Search</button>
+      </form>
+    </div>
+  )
+}
+
+export default SearchBar
 
