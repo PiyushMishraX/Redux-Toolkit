732d5efd2e6fd0c05d7a042efa37931adf057bc5
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-06 21:31:37 +0530

    base search bar

--- project/src/components/SearchBar.jsx
+++ project/src/components/SearchBar.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SearchBar = () => {
+
+    const [text, setText] = useState('')
+
+
+
   return (
     <div>
-      <form >
-        <input type="text" placeholder='Search anything..' />
-        <button>Search</button>
+      <form className="flex p-10 gap-5 bg-gray-900">
+
+        <input required
+        className=" border-2 px-4 py-2 text-xl rounded outline-none"
+        type="text" 
+        placeholder="Search anything.." 
+        />
+        <button 
+        className="active:scale-95 border-2 px-4 py-2 text-xl rounded outline-none cursor-pointer ">
+            Search
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
-
+export default SearchBar;
