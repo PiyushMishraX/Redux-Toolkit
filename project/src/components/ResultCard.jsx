d6b6c2dc5e3f2f998c8fcee606c2f433487b77be
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-07 18:30:19 +0530

    save in loca storage

--- project/src/components/ResultCard.jsx
+++ project/src/components/ResultCard.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
 
 // const ResultCard = (props) => {
 const ResultCard = ({item}) => {
+
+    const addToCOllection = (item)=>{
+        // console.log(item);
+
+        const oldData = JSON.parse(localStorage.getItem('collection')) || []
+        // console.log(oldData);
+
+        const newData = [...oldData,item]
+        // console.log(newData);
+
+        localStorage.setItem('collection',JSON.stringify(newData))
+        
+        
+        
+    }
+
   return (
     // <div className='w-[18vw] h-80 relative bg-white rounded'>
     // <div className='w-[18vw] min-w-fit h-80 relative bg-white rounded'>
@@ -21,10 +36,14 @@ const ResultCard = ({item}) => {
         <div id='bottom' className=' w-full px-4 py-6  text-white absolute bottom-0 flex justify-between  items-center gap-2'>
             {/* <h2 className=' text-xl font-semibold capitalize '>{item.title}</h2>   */}
             {/* <h2 className=' text-xs md:text-[13px] lg:text-[15px] font-semibold capitalize h-15  overflow-hidden truncate '>{item.title}</h2>   */}
-            <h2 className='text-[16px] md:text-[18px] lg:text-[20px] font-semibold capitalize h-14 flex items-center overflow-hidden leading-tight'>
+            <h2 className='text-[16px] md:text-[18px] lg:text-[20px] font-semibold capitalize h-14 flex items-center overflow-hidden'>
               {item.title}
             </h2>  
-            <button className='bg-indigo-600 text-white rounded px-3 py-2 cursor-pointer font-medium text-[14px] shrink-0 active:scale-95'>
+            <button
+            onClick={()=>{
+                addToCOllection(item)
+            }}
+             className='bg-indigo-600 text-white rounded px-3 py-2 cursor-pointer font-medium text-[14px] shrink-0 active:scale-95' >
               Save
             </button>       
         </div>
