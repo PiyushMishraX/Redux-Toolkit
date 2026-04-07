8f989cc80a3ffeb2b8f76dd5a1d424445b89bf08
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-07 16:24:19 +0530

    result card

--- project/src/components/ResultCard.jsx
+++ project/src/components/ResultCard.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 // const ResultCard = (props) => {
 const ResultCard = ({item}) => {
   return (
-    <div></div>
+    <div className='w-[18vw] h-80 bg-white rounded'>
+        {item.type == 'photo' ?<img src={item.src} alt="" />:''}
+        {item.type == 'video' ?<video autoPlay loop muted src="{item.src}"></video>:''}
+        {item.type == 'gif' ?<img src={item.src} alt="" />:''}
+        <h1 className='text-black'>{item.title}</h1>
+    </div>
   )
 }
 
